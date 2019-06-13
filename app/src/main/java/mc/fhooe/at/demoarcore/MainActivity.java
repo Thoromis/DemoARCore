93001f33bc3b2e155d37f2a1ce5411e6561eb6de
Thomas Maier <thomas.maier@tractive.com>
2019-06-13 21:03:55 +0200

    three models in app done and working

--- app/src/main/java/mc/fhooe/at/demoarcore/MainActivity.java
+++ app/src/main/java/mc/fhooe/at/demoarcore/MainActivity.java
@@ -3,29 +3,25 @@ package mc.fhooe.at.demoarcore;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
-import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ArFragment arFragment;
-    private ModelRenderable bearRenderable, dogRenderable;
+    private ModelRenderable bearRenderable, dogRenderable, hippoRenderable;
 
-    ImageView bear, dog;
+    ImageView bear, dog, hippo;
 
     View arrayView[];
     int selected = 1;
@@ -41,25 +37,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         bear = findViewById(R.id.bear);
         dog = findViewById(R.id.dog);
+        hippo = findViewById(R.id.hippo);
         setUpModel();
         setArrayView();
         setClickListener();
-        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
-            @Override
-            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
-                Anchor anchor = hitResult.createAnchor();
-                AnchorNode anchorNode = new AnchorNode(anchor);
-                anchorNode.setParent(arFragment.getArSceneView().getScene());
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-                createModel(anchorNode, selected);
+            createModel(anchorNode, selected);
 
-            }
         });
 
     }
 
     private void setUpModel() {
-
         ModelRenderable.builder()
                 .setSource(this, R.raw.bear)
                 .build().thenAccept(renderable -> bearRenderable = renderable)
@@ -76,6 +69,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .exceptionally(
                         throwable -> {
                             Toast.makeText(this, "Unable to load dog model", Toast.LENGTH_SHORT).show();
+                            return null;
+
+                        }
+                );
+        ModelRenderable.builder()
+                .setSource(this, R.raw.hippopotamus)
+                .build().thenAccept(renderable -> hippoRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast.makeText(this, "Unable to load hippo model", Toast.LENGTH_SHORT).show();
                             return null;
 
                         }
@@ -103,6 +106,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 addName(anchorNode, bear, "Dog");
             }
             break;
+            case 3: {
+                TransformableNode bear = new TransformableNode(arFragment.getTransformationSystem());
+                bear.setParent(anchorNode);
+                bear.setRenderable(hippoRenderable);
+                bear.select();
+
+                addName(anchorNode, bear, "Hippo");
+            }
+            break;
         }
     }
 
@@ -121,25 +133,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     TextView txt_name = (TextView) viewRenderable.getView();
                     txt_name.setText(name);
 
-                    txt_name.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            anchorNode.setParent(null);
-                        }
-                    });
+                    txt_name.setOnClickListener(view -> anchorNode.setParent(null));
                 });
 
     }
 
     private void setClickListener() {
-        for (int i = 0; i < arrayView.length; i++) {
-            arrayView[i].setOnClickListener(this);
+        for (View anArrayView : arrayView) {
+            anArrayView.setOnClickListener(this);
         }
     }
 
     private void setArrayView() {
         arrayView = new View[]{
-                bear, dog
+                bear, dog, hippo
         };
     }
 
@@ -148,22 +155,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.bear: {
                 selected = 1;
-                setBackbround(view.getId());
+                setBackground(view.getId());
             }
             break;
             case R.id.dog: {
-                selected = 4;
-                setBackbround(view.getId());
+                selected = 2;
+                setBackground(view.getId());
             }
+            break;
+            case R.id.hippo: {
+                selected = 3;
+                setBackground(view.getId());
+            }
+            break;
         }
     }
 
-    private void setBackbround(int id) {
-        for (int i = 0; i < arrayView.length; i++) {
-            if (arrayView[i].getId() == id) {
-                arrayView[i].setBackgroundColor(Color.parseColor("#80333639"));
+    private void setBackground(int id) {
+        for (View anArrayView : arrayView) {
+            if (anArrayView.getId() == id) {
+                anArrayView.setBackgroundColor(Color.parseColor("#80333639"));
             } else {
-                arrayView[i].setBackgroundColor(Color.TRANSPARENT);
+                anArrayView.setBackgroundColor(Color.TRANSPARENT);
             }
         }
     }
